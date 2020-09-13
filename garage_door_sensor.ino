5493dee94e63d95e9fe3860092bb47f249a1e92b
Marek <marek@invalid.org>
2020-09-13 19:41:33 -0400

    first prototype

--- garage_door_sensor.ino
+++ garage_door_sensor.ino
@@ -9,10 +9,18 @@
 const int trigPin = 2;  //D4
 const int echoPin = 0;  //D3
 
+// range for garage door open/closed (17-23 cm)
+const int garageOpenHigh = 23;
+const int garageOpenLow  = 17;
+
+enum DoorState { D_UNKNOWN, D_OPEN, D_CLOSED };
+enum DoorState doorState = D_UNKNOWN;
+
 // defines variables
 long duration;
 int distance;
 
+/** A helper method to blink the LED with specific on/off durations up to a max time */
 void blink(int onDuration, int offDuration, int total) {
   int timer = 0;
   while(timer < total) {
@@ -71,6 +79,9 @@ void setup() {
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.println("Setup complete");
   blink(50, 30, 200);
+
+  // https://blog.creations.de/?p=149
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
 }
 
 void loop() {
@@ -90,6 +101,16 @@ void loop() {
   // Calculating the distance (cm)
   distance= duration*0.034/2;
 
+  // Figure out door state
+  enum DoorState currentDoorState;
+  if(distance < garageOpenLow) {
+    currentDoorState = D_UNKNOWN;
+  } else if(distance >= garageOpenLow && distance <= garageOpenHigh) {
+    currentDoorState = D_OPEN;
+  } else {
+    currentDoorState = D_CLOSED;
+  }
+
   char distanceStr[33];
   itoa(distance, distanceStr, 10);
   
@@ -97,50 +118,68 @@ void loop() {
   Serial.print("Distance: ");
   Serial.println(distanceStr);
 
+  if(doorState != currentDoorState) {
+    // state change, need to notify!
+    // todo should connect to wifi only when notifying?
 
-  const int httpsPort = 443;
-  
-  WiFiClientSecure client;
-  Serial.print("Connecting to ");
-  Serial.println(apiHost);
- 
-  client.setInsecure();
-  
-  if (!client.connect(apiHost, httpsPort)) {
-    Serial.println("Connection failed");
-    return;
-  }
-
-  String url = "/dev/door";
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-
-  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
-               "Host: " + apiHost + "\r\n" +
-               "X-API-Key: " + apiKey + "\r\n" +
-               "doorstatus: " + distanceStr + "\r\n" +
-               "Content-Type: application/json\r\n" +
-               "User-Agent: ESP8266Tester\r\n" +
-               "Connection: close\r\n\r\n");
-
-  Serial.println("Request sent");
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    Serial.print("Got line: ");
-    Serial.println(line);
-    if (line == "\r") {
-      Serial.println("Headers received");
-      break;
+    const int httpsPort = 443;
+    
+    WiFiClientSecure client;
+    Serial.print("Connecting to ");
+    Serial.println(apiHost);
+   
+    client.setInsecure();
+    
+    if (!client.connect(apiHost, httpsPort)) {
+      Serial.println("Connection failed");
+      return;
     }
-  }
-  String line = client.readStringUntil('\n');
-  Serial.println("Reply was:");
-  Serial.println("==========");
-  Serial.println(line);
-  Serial.println("==========");
-  Serial.println("Closing connection");  
   
-  delay(60000);
+    String url = "/dev/door";
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
 
+    char *doorStateStr;
+    if(currentDoorState == D_OPEN) {
+      doorStateStr = "Garage door open";
+    } else if(currentDoorState == D_CLOSED) {
+      doorStateStr = "Garage door closed";
+    } else {
+      doorStateStr = "Unknown";
+      // todo include measurement?
+    }
+
+    char report[64];
+    snprintf(report, sizeof(report), "%s (%d)", doorStateStr, distance);
   
+    client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + apiHost + "\r\n" +
+                 "X-API-Key: " + apiKey + "\r\n" +
+                 "doorstatus: " + report + "\r\n" +
+                 "Content-Type: application/json\r\n" +
+                 "User-Agent: ESP8266GarageDoorSensor\r\n" +
+                 "Connection: close\r\n\r\n");
+  
+    Serial.println("Request sent");
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      Serial.print("Got line: ");
+      Serial.println(line);
+      if (line == "\r") {
+        Serial.println("Headers received");
+        break;
+      }
+    }
+    String line = client.readStringUntil('\n');
+    Serial.println("Reply was:");
+    Serial.println("==========");
+    Serial.println(line);
+    Serial.println("==========");
+    Serial.println("Closing connection");  
+
+    // status sent (todo should check if successful?), update door state
+    doorState = currentDoorState;
+  }
+
+  delay(60000);
 }
